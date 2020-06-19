ff717635576090906933be3b83826fc4f23a0065
Jasleen Kaur Chahal <jasleenk@geekyants.com>
2020-06-19 13:06:43 +0530

    typescript added

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Home from './src/screens/home';
+
+//disable yellow-block
+
+const App: () => React$Node = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Home />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
